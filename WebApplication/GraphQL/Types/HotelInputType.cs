e9da92f033a87c10c5aad0a31576ee669d2614b0
Sarah <Sarah_Bani@outlook.com>
2020-11-24 12:57:04 +0330

    untracked files on developer: e1851983 GraphQL fetch countries & related cities

---
+++ WebApplication/GraphQL/Types/HotelInputType.cs
@@ -0,0 +1,18 @@
+﻿using Core.DomainModel.Entities;
+using GraphQL.Types;
+
+namespace UserInterface.GraphQL.Types
+{
+    public class HotelInputType : InputObjectGraphType
+    {
+        public HotelInputType()
+        {
+            Name = "HotelInput";
+            Field<LongGraphType>(nameof(Hotel.Id));
+            Field<NonNullGraphType<StringGraphType>>(nameof(Hotel.Name));
+            Field<NonNullGraphType<LongGraphType>>(nameof(Hotel.CityId));
+            Field<NonNullGraphType<ByteGraphType>>(nameof(Hotel.Stars));
+            Field<StringGraphType>(nameof(Hotel.Address));
+        }
+    }
+}
---
+++ WebApplication/GraphQL/Types/TransactionResultType.cs
@@ -0,0 +1,18 @@
+﻿using Core.DomainService;
+using GraphQL.Types;
+
+namespace UserInterface.GraphQL.Types
+{
+    public class TransactionResultType : ObjectGraphType<TransactionResult>
+    {
+
+        public TransactionResultType()
+        {
+            Name = nameof(TransactionResult);
+
+            Field(q => q.IsSuccessful);
+            Field(q => q.CustomExceptionMessage);
+        }
+
+    }
+}
