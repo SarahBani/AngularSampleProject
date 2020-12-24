e6db1e3fe67e233e57818d68a38afa6cd5fd782f
Sarah <Sarah_Bani@outlook.com>
2020-12-24 16:07:30 +0330

    correct some bugs in login

--- WebApplication/ClientApp/src/app/services/auth-guard-service.ts
+++ WebApplication/ClientApp/src/app/services/auth-guard-service.ts
@@ -3,34 +3,39 @@ import { Observable } from "rxjs";
 import { Injectable } from "@angular/core";
 import { AuthService } from "./auth-service";
 
-@Injectable()
+@Injectable({ providedIn: 'root' })
 export class AuthGuardService implements CanActivate, CanActivateChild {
 
-    constructor(private authService: AuthService,
-        private router: Router) { }
+  constructor(private authService: AuthService,
+    private router: Router) { }
 
-    //canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean
-    //    | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
-    //    throw new Error("Method not implemented.");
-    //}
+  //canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean
+  //    | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
+  //    throw new Error("Method not implemented.");
+  //}
 
-    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot)
-        : boolean | Observable<boolean> | Promise<boolean> {
-        return this.authService.isAuthenticated()
-            .then((authenticated: boolean) => {
-                if (authenticated) {
-                    return true;
-                }
-                else {
-                    this.router.navigate(['/access-denied']);
-                }
-            });
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot)
+    : boolean | Observable<boolean> | Promise<boolean> {
+    //return this.authService.isAuthenticated()
+    //    .then((authenticated: boolean) => {
+    //        if (authenticated) {
+    //            return true;
+    //        }
+    //        else {
+    //            this.router.navigate(['/access-denied']);
+    //        }
+    //    });
+    if (this.authService.isAuthenticated()) {
+      return true;
     }
-
-    canActivateChild(childRoute: ActivatedRouteSnapshot, state: RouterStateSnapshot)
-        : boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
-        return this.canActivate(childRoute, state);
+    else {
+      this.router.navigate(['/access-denied']);
     }
+  }
 
+  canActivateChild(childRoute: ActivatedRouteSnapshot, state: RouterStateSnapshot)
+    : boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
+    return this.canActivate(childRoute, state);
+  }
 
 }
